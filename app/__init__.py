2cafb182f9c37dc3230eabd9b8da4d38cc4fe76b
Candia Gu <candi37@gmail.com>
2018-01-04 21:10:00 -0500

    added compatibility for googleapp

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask, url_for
 
 # Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True, static_folder='static')
 
 # Load the views
 from app import views
