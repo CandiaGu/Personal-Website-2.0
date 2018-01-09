38a4c042fb5d6dcde1f6d6e7a27df0d65b903739
Candia Gu <candi37@gmail.com>
2018-01-08 23:18:10 -0500

    updated projects page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,12 +1,28 @@
-$(function() {
-    $('a[href*="#"]:not([href="#carouselExampleControls"])').click(function() {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
-        if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    });
+$(document).ready(
+    function() {
+        
+        $("#persweb2" ).click(function() {
+            
+            $('#projdesc').html('\
+                <div class="row" ><h3>Personal Website V2</h3></div>\
+                <div class="row"><div class="col-1"></div>\
+                <div class="col-11" style = "border-left: solid 1px;"><p>This lightweight website we’re currently on, was made from scratch \
+                mostly using raw HTML, CSS, and Javascript, sprinkled with some jQuery \
+                for the animations and Bootstrap for the ease of formatting. I personally \
+                designed and developed this website to practice and learn web development \
+                as well as UI/UX design.</p>\
+                <p>Some other tools I learned to use through this were Flask, \
+                a python microframework, which I used for general templating (i.e. headers and footers) \
+                throughout the site as well as testing on my local environment, virtualenv \
+                and virtualenvwrapper to keep my workspace accessible and organized, and \
+                Google Cloud SDK to test and deploy my website.\
+                </p></div>');
+        });
+        $("#persweb2").click();
+        $( "#card2" ).click(function() {
+            $('#projdesc').html('<p>More replaced text.</p>');
+        });
+
+
 });
+
